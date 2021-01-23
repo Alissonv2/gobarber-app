07c052442add898bf5ddb8710c011f25b29411fd
Alissonv2 <alissonvitorino8@gmail.com>
2021-01-23 16:00:32 -0300

    Added authentication and async storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar backgroundColor="#312E38" barStyle="light-content" />
-    <View style={{ flex: 1, backgroundColor: '#312E38' }}>
-      <Routes />
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312E38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
